iamsj7/platform_build

core: Include our vendor config

core/config.mk
@@ -1176,4 +1176,6 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+include vendor/hentai/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+-include $(TOPDIR)vendor/hentai/build/core/pathmap.mk

StormBreakerOSS/android_build

Change-Id: I4296948ca9e04a0025881bc702142df02727da70

core/config.mk
@@ -1240,4 +1240,6 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+include vendor/stormbreaker/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+-include $(TOPDIR)vendor/stormbreaker/build/core/pathmap.mk